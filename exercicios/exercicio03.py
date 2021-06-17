0cd26455bb7cdfe6f268153dfd680b1c1465c028
Tiago <rocha.tpr@gmail.com>
2021-06-16 22:28:42 -0300

    Aula03

---
+++ exercicios/exercicio03.py
@@ -0,0 +1,42 @@
+#Nome
+#Idade
+#Email
+#Telefone
+
+#Apresentara o seguinte menu:
+
+#Bem vindo(a) nome
+
+#Opções
+
+# 1 - Mostrar todas suas informações
+# 2 - Mostrar apenas seu nome e idade
+# 3 - Mostrar seu email e telefone
+# 0 - Sair
+
+# nome = input("Digite seu nome:")
+# idade = input("Digite sua idade:")
+# email = input("Digite seu e-mail:")
+# telefone = input("Digite seu telefone:")
+
+
+# while True:
+#     print(f"Bem vindo {nome}!")
+#     print("1 - Mostrar todas suas informações")
+#     print("2 - Mostrar apenas seu nome e idade")
+#     print("3 - Mostrar seu email e telefone")
+#     print("0 - Sair")
+#     opcao = int(input('>>>>'))
+    
+#     if opcao == 1:
+#         print(f"Nome:{nome}\nIdade:{idade}\nE-mail:{email}\nTelefone:{telefone}")
+#     elif opcao == 2:
+#         print(f"Nome:{nome}\nIdade:{idade}")
+#     elif opcao == 3:
+#         print(f"Telefone:{telefone}\nE-mail:{email}")
+#     elif opcao == 0:
+#         break
+#     else:
+#         print("Opção Inválida!")
+    
+
